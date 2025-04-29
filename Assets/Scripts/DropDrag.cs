6d9b531aca0d29249ee1bd052f4acf3684707ace
mevlutucar <ucarcodes@gmail.com>
2025-04-29 09:50:54 +0300

    Sahne ve Assetler olusturuldu
    Kullanilacak assetler belirlendi. Sahne olusturuldu ve tasarım surecleri devam ediyor.

---
+++ Assets/Scripts/DropDrag.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
