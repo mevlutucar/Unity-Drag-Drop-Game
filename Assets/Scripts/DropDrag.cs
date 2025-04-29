02a27c74aea94d74f0f9ff5ea645096d92e6c804
mevlutucar <ucarcodes@gmail.com>
2025-04-29 11:12:45 +0300

    Drag drop scripti olusturuldu
    Sahnedeki gorsellerin surukle-birak sisteminin calismasi icin gereken script olusturuldu.

--- Assets/Scripts/DropDrag.cs
+++ Assets/Scripts/DropDrag.cs
@@ -3,18 +3,32 @@ using UnityEngine.EventSystems;
 
 public class DropDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    RectTransform rectTransform;
+    Canvas canvas;
+    CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>(); //Parent nesnesinin canvasina ulastik.
+
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        canvasGroup.alpha = 0.8f;
+        canvasGroup.blocksRaycasts = false; //nesnenin tiklanabilirlik ozelligini kapat.
+        Debug.Log("Nesne taþýnmaya baþlandý.");
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
     }
 }
